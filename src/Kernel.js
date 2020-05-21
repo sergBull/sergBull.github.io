4bd03f592fd3090b3396e1129990f930f11c6a46
sergBull <bullsergv@gmail.com>
2020-05-21 14:36:37 +0300

    Update Kernel.js

--- src/Kernel.js
+++ src/Kernel.js
@@ -13,6 +13,7 @@ var Terminal = (function() {
 	function Terminal() {
 		this._ = private();
 		this._(key).Canvas = document.createElement("canvas");
+		this._(key).ctx = Canvas.getContext("2d");
 		
 		this.bgColor = 0;
 		this.fgColor = 0;
@@ -22,7 +23,16 @@ var Terminal = (function() {
 	}
 	
 	Terminal.prototype.init = function(w, h) {
-		alert(w * h);
+		this.height = h;
+		this.width = w;
+		
+		var Canvas = this._(key).Canvas;
+		var ctx = this._(key).ctx;
+		
+		Canvas.height = this.height;
+		Canvas.width = this.width;
+		
+		ctx.fillRect(0, 0, this.width, this.height);
 	};
 	
 	return Terminal;
